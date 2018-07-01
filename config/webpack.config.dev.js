5d90d91d0fe743426fecf884832e50e5a225a642
fengzhiziLy <Ly332883899@163.com>
2018-07-02 02:30:58 +0800

    header

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -191,7 +191,7 @@ module.exports = {
                 options: {
                   modules: false,
                   modifyVars: {
-                    "@primary-color": "#f9c700"
+                    "@primary-color": "#33ccff"
                   }
                 }
               }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Life from './pages/demo/Life';
+// import Life from './pages/demo/Life';
+import Admin from './admin'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Life />, document.getElementById('root'));
+ReactDOM.render(<Admin />, document.getElementById('root'));
 registerServiceWorker();
