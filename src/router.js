eef244b289ed69c80eebd2c98b8aebd4f357307a
fengzhiziLy <Ly332883899@163.com>
2018-07-03 18:48:08 +0800

    form

--- src/router.js
+++ src/router.js
@@ -12,6 +12,8 @@ import Message from './pages/ui/messages'
 import Tabs from './pages/ui/tabs'
 import Gallery from './pages/ui/gallery'
 import Carousel from './pages/ui/carousel'
+import FormLogin from './pages/form/login'
+import FormRegister from './pages/form/register'
 
 export default class IRouter extends React.Component {
 
@@ -31,6 +33,8 @@ export default class IRouter extends React.Component {
                 <Route path="/admin/ui/tabs" component={Tabs} />
                 <Route path="/admin/ui/gallery" component={Gallery} />
                 <Route path="/admin/ui/carousel" component={Carousel} />
+                <Route path="/admin/form/login" component={FormLogin} />
+                <Route path="/admin/form/reg" component={FormRegister} />
                 <Route component={NoMatch} />
               </Switch>
             </Admin>
