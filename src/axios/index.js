36f7487ce3ac13d3a564fa42cfceed60988909a2
fengzhiziLy <Ly332883899@163.com>
2018-07-06 13:17:35 +0800

    city

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,9 +1,11 @@
 import JsonP from 'jsonp'
 import axios from 'axios'
-import { Modal } from 'antd'
+import {
+  Modal
+} from 'antd'
 
 export default class Axios {
-  static jsonp (options) {
+  static jsonp(options) {
     return new Promise((resolve, reject) => {
       JsonP(options.url, {
         param: 'callback'
@@ -17,29 +19,29 @@ export default class Axios {
     })
   }
 
-  static ajax (options) {
-    let loading
+  static ajax(options) {
+    let loading;
     if (options.data && options.data.isShowLoading !== false) {
-      loading = document.getElementById('ajaxLoading')
-      loading.style.display = 'block'
+      loading = document.getElementById('ajaxLoading');
+      loading.style.display = 'block';
     }
-    let baseApi = ' https://www.easy-mock.com/mock/5b3c4a5fa27b9a779117e80a/api'
+    let baseApi = 'https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api';
     return new Promise((resolve, reject) => {
       axios({
         url: options.url,
         method: 'get',
         baseURL: baseApi,
-        timeout: 10000,
+        timeout: 5000,
         params: (options.data && options.data.params) || ''
       }).then((response) => {
         if (options.data && options.data.isShowLoading !== false) {
-          loading = document.getElementById('ajaxLoading')
-          loading.style.display = 'none'
+          loading = document.getElementById('ajaxLoading');
+          loading.style.display = 'none';
         }
-        if (response.status === 200) {
-          let res = response.data
-          if (res.code === 0) {
-            resolve(res)
+        if (response.status == '200') {
+          let res = response.data;
+          if (res.code == '0') {
+            resolve(res);
           } else {
             Modal.info({
               title: "提示",
@@ -47,9 +49,9 @@ export default class Axios {
             })
           }
         } else {
-          reject(response.data)
+          reject(response.data);
         }
       })
-    })
+    });
   }
 }
